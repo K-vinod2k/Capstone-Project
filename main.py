e20a69e16a2102d457dfdadbae037fc09f2d23ce
Hyun <105446294+kimhw99@users.noreply.github.com>
2026-04-11 00:32:31 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from kim_workspace.robot_persona.src import RobotPersona
+from kim_workspace.stt.src import STT
+import sounddevice as sd
+import os
+
+def pickle_to_movement(path):
+
+    """
+    
+    Implement function that opens the robot pickle file
+    and converts it into robot movement
+
+    Input:
+    path: str (directory to movement pickle file)
+
+    """
+
+    # YOUR CODE HERE
+
+    print("[PICKLE TO ROBOT MOVEMENT: TODO]")
+
+if __name__ == "__main__":
+
+    # Input Variables
+    persona = "a Superhero"
+    movement_directory = './kim_workspace/movements'
+
+    # Models
+    HF_TOKEN = os.getenv('HF_TOKEN')
+    persona = RobotPersona(persona, movement_directory)
+    stt = STT()
+    stream = sd.InputStream(samplerate=stt.SAMPLERATE, 
+                        channels=1, 
+                        callback=stt.audio_callback)
+
+    with stream:
+        stt.process_audio(persona.forward, pickle_to_movement)
